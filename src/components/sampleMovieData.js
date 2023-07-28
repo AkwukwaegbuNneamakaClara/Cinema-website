a70e7d54cc0f519b573ffb73ff537f3c63320a62
Clara <nneamakaclara13@gmail.com>
2023-07-28 02:48:06 +0200

    Add sample details, and modify index.js

--- src/components/sampleMovieData.js
+++ src/components/sampleMovieData.js
@@ -11,6 +11,72 @@ const sampleMovies = [
       poster: '/images/lord-of-the-ring.jpg',
       length: '3 hours',
     },
+    {
+      id: 3,
+      title: 'Indiana Jones',
+      poster: '/images/indiana jones.jpg',
+      length: '1h 45m'
+    },
+    {
+      id: 4,
+      title: 'Movie Title',
+      poster: '/images/movie title.jpg',
+      length: '1h 5m'
+    },
+    {
+      id: 5,
+      title: 'Asteroid City',
+      poster: '/images/asteroid-city.jpg',
+      length: '2h 5m'
+    },
+    {
+      id: 6,
+      title: 'Pulp Fiction',
+      poster: '/images/pulp-fiction.jpg',
+      length: '1h 30m'
+    },
+    {
+      id: 7,
+      title: 'The thing',
+      poster: '/images/the thing.jpg',
+      length: '2 hours'
+    },
+    {
+      id: 8,
+      title: 'Top Gun',
+      poster: '/images/top-gun_.jpg',
+      length: '2h 25m'
+    },
+    {
+      id: 9,
+      title: 'Lion Heart',
+      poster: '/images/lionheart.jpg',
+      length: '4 hours'
+    },
+    {
+      id: 10,
+      title: 'True Grit',
+      poster: '/images/true grit.png',
+      length: '3h 2m'
+    },
+    {
+      id: 11,
+      title: 'X-Men The Last Stand',
+      poster: '/images/x-men-the-last-stand.jpg',
+      length: '2h 40m'
+    },
+    {
+      id: 12,
+      title: '76',
+      poster: '/images/76.jpg',
+      length: '4h 30m'
+    },
+    {
+      id: 13,
+      title: 'We Cant All Die First',
+      poster: '/images/we cant all die first.jpg',
+      length: '1h 5m'
+    },
     // Add more movie objects as needed
   ];
   
