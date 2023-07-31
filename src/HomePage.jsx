b403837fbc379aeb186a88d00a9b026dd80e8377
Clara <nneamakaclara13@gmail.com>
2023-07-31 20:41:05 +0200

    Modify HomePage by importing some components

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import Title from './components/Title/Title'; // Adjust the import path
 
@@ -8,28 +8,46 @@ import BookingPage from './components/BookingPage/BookingPage';
 import ScreeningList from './components/ScreeningList/ScreeningList'; // Import the ScreeningList component
 import sampleMovies from './components/sampleMovieData'; // Import the sampleMovies data
 import sampleScreeningListData from './components/sampleScreeningListData'; // Import the sampleScreenings data
+import CategoryFilter from './components/CategoryFilter/CategoryFilter'; // Import the CategoryFilter component
 
 import './HomePage.css'; // Import the app.css file
 
 
 
 function HomePage() {
+  const [selectedCategory, setSelectedCategory] = useState('all');
+  const allCategories = ['all', 'comedy', 'action', 'drama'];
+
+  const handleCategorySelect = (category) => {
+    console.log('Selected category:', category);
+    setSelectedCategory(category);
+  };
+  // Filter the movieDetails data based on the selected category
+  const filteredMovies =
+    selectedCategory === 'all'
+      ? sampleScreeningListData // Show all movies if 'All Categories' is selected
+      : sampleScreeningListData.filter((screening) => screening.category === selectedCategory);
+
   return (
     <Router>
       <div>
         <Title /> {/* This line includes the Title component */}
-        <h1>Now Showing</h1>
-        {/* Add Links for navigation */}
         <Link to="/">Home</Link>
-        <Link to="/movies">Movie List</Link>
-        <Link to="/booking">Booking</Link>
+<Link to="/movies">Movie List</Link>
+<Link to="/booking">Booking</Link>
         <Switch>
         <Route exact path="/" render={() => <MovieList movies={sampleMovies} />} />
           <Route path="/movies/:id" component={MovieDetails} />
           <Route path="/booking/:id" component={BookingPage} />
           {/* Add more routes for other pages if needed */}
         </Switch>
-        <ScreeningList screenings={sampleScreeningListData} />{/* pass your screening data here */} 
+        <div>
+      {/* Render the CategoryFilter component and pass the necessary props */}
+      <CategoryFilter categories={allCategories} onSelectCategory={handleCategorySelect} />
+
+      {/* Render the ScreeningList component and pass the necessary props */}
+      <ScreeningList screenings={filteredMovies} />
+    </div>
       </div>
     </Router>
   );
