fafb0b01c20ba378b00408b3b1188f872e06edc4
Clara <nneamakaclara13@gmail.com>
2023-07-31 20:43:32 +0200

    Added some movie details

--- src/components/sampleScreeningListData.js
+++ src/components/sampleScreeningListData.js
@@ -1,23 +1,126 @@
-// sampleScreenings.js
+// sampleScreeningListData.js
 const sampleScreeningListData = [
     {
       id: 1,
       date: '2023-08-30',
       time: '18:30',
-      title: 'Movie 1',
-      poster: '/path/to/poster1.jpg',
-      length: '2h 15m',
+      title: 'Titanic',
+      category: 'Drama',
+      poster: '/images/titanic.jpg',
+      length: '3h 5m',
     },
     {
       id: 2,
       date: '2023-09-02',
       time: '21:00',
-      title: 'Movie 2',
-      poster: '/path/to/poster2.jpg',
-      length: '1h 45m',
+      title: 'Lord Of The Ring',
+      category: 'Drama',
+      poster: '/images/lord-of-the-ring.jpg',
+      length: '3 hours',
     },
-    // Add more screening objects as needed
+    {
+      id: 3,
+      date: '2023-09-04',
+      time: '20:00',
+      title: 'Indiana Jones',
+      category: 'Action',
+      poster: '/images/indiana jones.jpg',
+      length: '1h 45m'
+    },
+    {
+      id: 4,
+      date: '2023-09-06',
+      time: '18:30',
+      title: 'Movie Title',
+      category: 'Comedy',
+      poster: '/images/movie title.jpg',
+      length: '1h 5m'
+    },
+    {
+      id: 5,
+      date: '2023-09-08',
+      time: '19:00',
+      title: 'Asteroid City',
+      category: 'Action',
+      poster: '/images/asteroid-city.jpg',
+      length: '2h 5m'
+    },
+    {
+      id: 6,
+      date: '2023-09-10',
+      time: '20:30',
+      title: 'Pulp Fiction',
+      category: 'Action',
+      poster: '/images/pulp-fiction.jpg',
+      length: '1h 30m'
+    },
+    {
+      id: 7,
+      date: '2023-09-12',
+      time: '21:00',
+      title: 'The thing',
+      poster: '/images/the thing.jpg',
+      length: '2 hours'
+    },
+    {
+      id: 8,
+      date: '2023-09-14',
+      time: '21:30',
+      title: 'Top Gun',
+      poster: '/images/top-gun_.jpg',
+      length: '2h 25m'
+    },
+    {
+      id: 9,
+      date: '2023-09-16',
+      time: '18:00',
+      title: 'Lion Heart',
+      poster: '/images/lionheart.jpg',
+      length: '4 hours'
+    },
+    {
+      id: 10,
+      date: '2023-09-18',
+      time: '18:30',
+      title: 'True Grit',
+      poster: '/images/true grit.png',
+      length: '3h 2m'
+    },
+    {
+      id: 11,
+      date: '2023-09-20',
+      time: '19:00',
+      title: 'X-Men The Last Stand',
+      poster: '/images/x-men-the-last-stand.jpg',
+      length: '2h 40m'
+    },
+    {
+      id: 12,
+      date: '2023-09-22',
+      time: '19:30',
+      title: '76',
+      poster: '/images/76.jpg',
+      length: '4h 30m'
+    },
+    {
+      id: 13,
+      date: '2023-09-24',
+      time: '20:00',
+      title: 'We Cant All Die First',
+      poster: '/images/we cant all die first.jpg',
+      length: '1h 5m'
+    },
+    {
+      id: 14,
+      date: '2023-08-30',
+      time: '21:30',
+      title: '762',
+      poster: '/images/762.jpg',
+      length: '3h 5m',
+    }
+      // Add more movie objects as needed
   ];
+    
   
   export default sampleScreeningListData;
   
